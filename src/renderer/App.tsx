d45bc0e870ecd936a087a83bed57ecad93d6feac
oSethoum@gmail.com <os.sethoum@gmail.com>
2022-04-05 14:00:14 +0100

    drop react-router-dom in favor of react-location

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,17 +1,29 @@
 import { Box } from "@mantine/core";
 import { Home, StatusBar, ToolBar } from "./ui";
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  createBrowserHistory,
+  Router,
+  ReactLocation,
+  Route,
+} from "@tanstack/react-location";
+
+const history = createBrowserHistory();
+
+const location = new ReactLocation({ history });
+
+const routes: Route[] = [
+  {
+    path: "/",
+    element: <Home />,
+  },
+];
 
 function App() {
   return (
     <Box sx={{ height: "100vh", display: "flex", flexDirection: "column" }}>
       <ToolBar />
       <Box sx={{ flexGrow: 1 }}>
-        <Router>
-          <Routes>
-            <Route path="/" element={<Home />} />
-          </Routes>
-        </Router>
+        <Router location={location} routes={routes}></Router>
       </Box>
       <StatusBar />
     </Box>
